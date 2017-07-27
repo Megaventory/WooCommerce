dd001f3631d2e30fa46191465563068c4f30951f
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2017-07-27 15:45:05 +0300

    run cron every 5 mins

--- cron.php
+++ cron.php
@@ -5,9 +5,6 @@ function cron_activation(){
     if(!wp_next_scheduled('pull_changes_event')){
         wp_schedule_event(time(), '5min', 'pull_changes_event');
     }
-    if(!wp_next_scheduled('pull_stock_event')){
-        //wp_schedule_event(time(), '5min', 'pull_stock_event');
-    }
 }
 // The deactivation hook
 function cron_deactivation(){
@@ -22,7 +19,7 @@ function cron_deactivation(){
 // every 5 mins
 function schedule($schedules) {
     $schedules['5min'] = array(
-            'interval'  => 30, //30 secs for debug //5 * 60, //5min
+            'interval'  => 5 * 60, //30 secs for debug //5 * 60, //5min
             'display'   => __('Every 5 Minutes', 'textdomain')
     );
     return $schedules;
